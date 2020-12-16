3ec91b52852c898dd124e77e56b5bfec89ae309e
Yurii Trach <yurko2000@gmail.com>
2020-12-16 23:13:07 +0200

    added menu router and DB

--- src/server.js
+++ src/server.js
@@ -1,13 +1,35 @@
 const express = require("express");
-const path = require('path');
 const app = express();
-const modelViewerRouter = require("./Routers/model-viewer");
+const mongoose = require("mongoose");
+require("dotenv/config")
 
+//routers import
+const modelViewerRouter = require("./Routers/model-viewer");
+const menuRouter = require("./Routers/menu");
+//routers used
 app.use("/viewer", modelViewerRouter);
+app.use("/menu", menuRouter);
 
 app.get("/", (req, res) => {
     res.send("We are on home");
 });
 
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dqdjs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    {useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("connected to db")
+    }
+})
+
+
+
+
 //Listening
-app.listen(8000, () => {});
+app.listen(8000, () => {
+    console.log("server started")
+});
