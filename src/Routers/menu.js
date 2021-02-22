2751415c6379113ff1ba836d1546d6c8b0586ad1
Yurii Trach <ytrach@tsukat.com>
2021-02-23 01:09:14 +0200

    final

--- src/Routers/menu.js
+++ src/Routers/menu.js
@@ -4,9 +4,11 @@ const Menu = require("../models/MenuModel")
 
 router.get("/", async (req, res, next) => {
     try{
+        console.log(1)
         const currentMenu = await Menu.findOne({current: true})
         res.status(200).json(currentMenu)
     } catch (err) {
+        console.log(err)
         res.status(500).json({message: ["Internal server error"]})
     }
 });
